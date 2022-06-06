8d627db75d28abeb02d82f5ba7146ef35c7b3dc3
giusepasian <83140249+giusepasian@users.noreply.github.com>
2022-06-06 12:14:56 -0300

    Add files via upload

---
+++ inmobiliaria-mgp/js/script.js
@@ -0,0 +1,140 @@
+document.getElementById("icon-menu").addEventListener("click", mostrar_menu);
+
+function mostrar_menu(){
+
+    document.getElementById("move-content").classList.toggle('move-container-all');
+    document.getElementById("show-menu").classList.toggle('show-lateral');
+}
+
+
+
+
+
+
+
+
+                            //Buscador de contenido
+
+
+//Ejecutando funciones
+document.getElementById("icon-search").addEventListener("click", mostrar_buscador);
+document.getElementById("cover-ctn-search").addEventListener("click", ocultar_buscador);
+
+//Declarando variables
+bars_search =       document.getElementById("ctn-bars-search");
+cover_ctn_search =  document.getElementById("cover-ctn-search");
+inputSearch =       document.getElementById("inputSearch");
+box_search =        document.getElementById("box-search");
+
+
+//Funcion para mostrar el buscador
+function mostrar_buscador(){
+
+    bars_search.style.top = "80px";
+    cover_ctn_search.style.display = "block";
+    inputSearch.focus();
+
+    if (inputSearch.value === ""){
+        box_search.style.display = "none";
+    }
+
+}
+
+//Funcion para ocultar el buscador
+function ocultar_buscador(){
+
+    bars_search.style.top = "-10px";
+    cover_ctn_search.style.display = "none";
+    inputSearch.value = "";
+    box_search.style.display = "none";
+
+}
+
+
+//Creando filtrado de busqueda
+
+document.getElementById("inputSearch").addEventListener("keyup", buscador_interno);
+
+function buscador_interno(){
+
+
+    filter = inputSearch.value.toUpperCase();
+    li = box_search.getElementsByTagName("li");
+
+    //Recorriendo elementos a filtrar mediante los "li"
+    for (i = 0; i < li.length; i++){
+
+        a = li[i].getElementsByTagName("a")[0];
+        textValue = a.textContent || a.innerText;
+
+        if(textValue.toUpperCase().indexOf(filter) > -1){
+
+            li[i].style.display = "";
+            box_search.style.display = "block";
+
+            if (inputSearch.value === ""){
+                box_search.style.display = "none";
+            }
+
+        }else{
+            li[i].style.display = "none";
+        }
+
+    }
+
+
+
+}
+ // CONTACTO
+$(document).ready(function(){
+
+    $('#btnSend').click(function(){
+
+        var errores = '';
+
+        // Validado Nombre ==============================
+        if( $('#names').val() == '' ){
+            errores += '<p>Escriba un nombre</p>';
+            $('#names').css("border-bottom-color", "#F14B4B")
+        } else{
+            $('#names').css("border-bottom-color", "#d1d1d1")
+        }
+
+        // Validado Correo ==============================
+        if( $('#email').val() == '' ){
+            errores += '<p>Ingrese un correo</p>';
+            $('#email').css("border-bottom-color", "#F14B4B")
+        } else{
+            $('#email').css("border-bottom-color", "#d1d1d1")
+        }
+
+        // Validado Mensaje ==============================
+        if( $('#mensaje').val() == '' ){
+            errores += '<p>Escriba un mensaje</p>';
+            $('#mensaje').css("border-bottom-color", "#F14B4B")
+        } else{
+            $('#mensaje').css("border-bottom-color", "#d1d1d1")
+        }
+
+        // ENVIANDO MENSAJE ============================
+        if( errores == '' == false){
+            var mensajeModal = '<div class="modal_wrap">'+
+                                    '<div class="mensaje_modal">'+
+                                        '<h3>Errores encontrados</h3>'+
+                                        errores+
+                                        '<span id="btnClose">Cerrar</span>'+
+                                    '</div>'+
+                                '</div>'
+
+            $('body').append(mensajeModal);
+        }
+
+        // CERRANDO MODAL ==============================
+        $('#btnClose').click(function(){
+            $('.modal_wrap').remove();
+        });
+    });
+
+});
+
+
